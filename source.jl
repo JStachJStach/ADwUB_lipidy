e8c798ee5c6a4f8a0cb34cb75ac0d62357069fb8
jakubstach <jakub.stach1102@gmail.com>
2026-04-26 15:09:22 +0200

    fix

--- source.jl
+++ source.jl
@@ -16,7 +16,7 @@ end
 kalibracja[!, :g1_średnia_wiersze] = mean(Matrix(kalibracja[!, [:g1_s1, :g1_s2]]), dims=2)[:, 1]
 kalibracja[!, :g2_średnia_wiersze] = mean(Matrix(kalibracja[!, [:g2_s1, :g2_s2]]), dims=2)[:, 1]
 kalibracja[!, :g1g2_średnia_wiersze] = mean(Matrix(kalibracja[!, [:g2_s1, :g2_s2, :g1_s1, :g1_s2]]), dims=2)[:, 1]
-kalibracja[!, :próbka] = [0, 50, 100, 200, 300, 400, 500]
+kalibracja[!, :próbka] = [0, 1, 2, 4, 6, 8, 10]
 
 function regresja_liniowa(x, y)
     x_śr = mean(x)
@@ -40,7 +40,7 @@ function wykres(ax, y, x, test_x)
     test_x_round = collect(skipmissing(round.(test_x, digits=2)))
     test_y_round = collect(skipmissing(round.(test_y, digits=2)))
     for (i, j) in zip(test_x_round, test_y_round)
-        info *= "$i -> $(j)μl\n"
+        info *= "$i -> $(j)μM\n"
     end
     vlines!(ax, test_x, linewidth=1.2, linestyle=:dash, color=:red, alpha=0.7)
     lines!(ax, linspace, a .* linspace .+ b, linestyle=(:dash, :dense), label="Krzywa kalibracyjna:\nr²=" * string(round(r2, digits=3)), linewidth=4)
@@ -51,17 +51,17 @@ end
 bkg_col = :gray95
 
 fig = Figure(size=(1200, 600))
-ax = Axis(fig[1, 1], title="grupa1 sesja1", backgroundcolor=bkg_col, yticks=kalibracja[:, :próbka], ylabel="próbka", ytickformat=v -> ["$(v)μl" for v in kalibracja[:, :próbka]])
+ax = Axis(fig[1, 1], title="grupa1 sesja1", backgroundcolor=bkg_col, yticks=kalibracja[:, :próbka], ylabel="próbka", ytickformat=v -> ["$(v)μM" for v in kalibracja[:, :próbka]])
 wykres(ax, kalibracja[:, :próbka], kalibracja[:, :g1_s1], test[:, :g1_s1])
-ax = Axis(fig[1, 2], title="grupa1 sesja2", backgroundcolor=bkg_col, yticks=kalibracja[:, :próbka], ytickformat=v -> ["$(v)μl" for v in kalibracja[:, :próbka]])
+ax = Axis(fig[1, 2], title="grupa1 sesja2", backgroundcolor=bkg_col, yticks=kalibracja[:, :próbka], ytickformat=v -> ["$(v)μM" for v in kalibracja[:, :próbka]])
 wykres(ax, kalibracja[:, :próbka], kalibracja[:, :g1_s2], test[:, :g1_s2])
-ax = Axis(fig[2, 1], title="grupa2 sesja1", backgroundcolor=bkg_col, yticks=kalibracja[:, :próbka], ylabel="próbka", xlabel="absorbancja względna", ytickformat=v -> ["$(v)μl" for v in kalibracja[:, :próbka]])
+ax = Axis(fig[2, 1], title="grupa2 sesja1", backgroundcolor=bkg_col, yticks=kalibracja[:, :próbka], ylabel="próbka", xlabel="absorbancja względna", ytickformat=v -> ["$(v)μM" for v in kalibracja[:, :próbka]])
 wykres(ax, kalibracja[:, :próbka], kalibracja[:, :g2_s1], test[:, :g2_s1])
-ax = Axis(fig[2, 2], title="grupa2 sesja2", backgroundcolor=bkg_col, yticks=kalibracja[:, :próbka], xlabel="absorbancja względna", ytickformat=v -> ["$(v)μl" for v in kalibracja[:, :próbka]])
+ax = Axis(fig[2, 2], title="grupa2 sesja2", backgroundcolor=bkg_col, yticks=kalibracja[:, :próbka], xlabel="absorbancja względna", ytickformat=v -> ["$(v)μM" for v in kalibracja[:, :próbka]])
 wykres(ax, kalibracja[:, :próbka], kalibracja[:, :g2_s2], test[:, :g2_s2])
-ax = Axis(fig[1, 3], title="grupa1 średnia", backgroundcolor=bkg_col, yticks=kalibracja[:, :próbka], ytickformat=v -> ["$(v)μl" for v in kalibracja[:, :próbka]])
+ax = Axis(fig[1, 3], title="grupa1 średnia", backgroundcolor=bkg_col, yticks=kalibracja[:, :próbka], ytickformat=v -> ["$(v)μM" for v in kalibracja[:, :próbka]])
 wykres(ax, kalibracja[:, :próbka], kalibracja[:, :g1_średnia_wiersze], vcat(test[:, :g1_s1], test[:, :g1_s2]))
-ax = Axis(fig[2, 3], title="grupa2 średnia", backgroundcolor=bkg_col, yticks=kalibracja[:, :próbka], xlabel="absorbancja względna", ytickformat=v -> ["$(v)μl" for v in kalibracja[:, :próbka]])
+ax = Axis(fig[2, 3], title="grupa2 średnia", backgroundcolor=bkg_col, yticks=kalibracja[:, :próbka], xlabel="absorbancja względna", ytickformat=v -> ["$(v)μM" for v in kalibracja[:, :próbka]])
 wykres(ax, kalibracja[:, :próbka], kalibracja[:, :g2_średnia_wiersze], vcat(test[:, :g2_s1], test[:, :g2_s2]))
 #display(fig)
 save("plot1.png", fig)
@@ -82,13 +82,13 @@ end
 
 bez_punktów = DataFrame([[i != optymalizacja_r2(col, kalibracja[:, :próbka]) ? col[i] : missing for i in 1:length(col)] for col in eachcol(kalibracja[:, 1:4])], names(kalibracja[:, 1:4]))
 fig = Figure(size=(800, 800))
-ax = Axis(fig[1, 1], title="grupa1 seria1", backgroundcolor=bkg_col, yticks=kalibracja[:, :próbka], ylabel="próbka", ytickformat=v -> ["$(v)μl" for v in kalibracja[:, :próbka]])
+ax = Axis(fig[1, 1], title="grupa1 seria1", backgroundcolor=bkg_col, yticks=kalibracja[:, :próbka], ylabel="próbka", ytickformat=v -> ["$(v)μM" for v in kalibracja[:, :próbka]])
 wykres(ax, kalibracja[:, :próbka], bez_punktów[:, :g1_s1], test[:, :g1_s1])
-ax = Axis(fig[1, 2], title="grupa1 seria2", backgroundcolor=bkg_col, yticks=kalibracja[:, :próbka], ytickformat=v -> ["$(v)μl" for v in kalibracja[:, :próbka]])
+ax = Axis(fig[1, 2], title="grupa1 seria2", backgroundcolor=bkg_col, yticks=kalibracja[:, :próbka], ytickformat=v -> ["$(v)μM" for v in kalibracja[:, :próbka]])
 wykres(ax, kalibracja[:, :próbka], bez_punktów[:, :g1_s2], test[:, :g1_s2])
-ax = Axis(fig[2, 1], title="grupa2 seria1", backgroundcolor=bkg_col, yticks=kalibracja[:, :próbka], xlabel="absorbancja względna", ylabel="próbka", ytickformat=v -> ["$(v)μl" for v in kalibracja[:, :próbka]])
+ax = Axis(fig[2, 1], title="grupa2 seria1", backgroundcolor=bkg_col, yticks=kalibracja[:, :próbka], xlabel="absorbancja względna", ylabel="próbka", ytickformat=v -> ["$(v)μM" for v in kalibracja[:, :próbka]])
 wykres(ax, kalibracja[:, :próbka], bez_punktów[:, :g2_s1], test[:, :g2_s1])
-ax = Axis(fig[2, 2], title="grupa2 seria2", backgroundcolor=bkg_col, yticks=kalibracja[:, :próbka], xlabel="absorbancja względna", ytickformat=v -> ["$(v)μl" for v in kalibracja[:, :próbka]])
+ax = Axis(fig[2, 2], title="grupa2 seria2", backgroundcolor=bkg_col, yticks=kalibracja[:, :próbka], xlabel="absorbancja względna", ytickformat=v -> ["$(v)μM" for v in kalibracja[:, :próbka]])
 wykres(ax, kalibracja[:, :próbka], bez_punktów[:, :g2_s2], test[:, :g2_s2])
 #display(fig)
 save("plot3.png", fig)
@@ -96,6 +96,6 @@ save("plot3.png", fig)
 
 bez_punktów[!, :g1g2_średnia_wiersze] = [mean(skipmissing(v)) for v in eachrow(bez_punktów)]
 fig = Figure()
-ax = Axis(fig[1, 1], title="Średnie wartości pomiarów dla krzywej kalibracyjnej we wszystkich seriach.\nUwzględniono również średnie z testowych dla grupy2 oraz grupy1 serii1", backgroundcolor=bkg_col, yticks=kalibracja[:, :próbka], ylabel="próbka", ytickformat=v -> ["$(v)μl" for v in kalibracja[:, :próbka]])
+ax = Axis(fig[1, 1], title="Średnie wartości pomiarów dla krzywej kalibracyjnej we wszystkich seriach.\nUwzględniono również średnie z testowych dla grupy2 oraz grupy1 serii1", backgroundcolor=bkg_col, yticks=kalibracja[:, :próbka], ylabel="próbka", ytickformat=v -> ["$(v)μM" for v in kalibracja[:, :próbka]])
 wykres(ax, kalibracja[:, :próbka], bez_punktów[:, :g1g2_średnia_wiersze], [mean(test[:, :g2_s1]), mean(test[:, :g2_s2]), mean(test[:, :g1_s1])])
 save("plot4.png", fig)
